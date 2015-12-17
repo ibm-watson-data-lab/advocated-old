707d139be67e36c0bbd036463362525b959da0a1
Glynn Bird <glynn.bird@gmail.com>
2015-12-17 16:27:14 +0000

    added token authentication

--- README.md
+++ README.md
@@ -5,21 +5,28 @@ A simple Node.js App that records CDS Developer Advocate activity in a Cloudant 
 It: 
 
 1. Responds to `POST /slack` commands which create provisional entries in the database
-2. Responds to `GET /doc?id=xxx` commands which fetch a single entry
-3. Responds to `POST /doc?id=xxx` API commands which updates an entry
+2. Responds to `GET /auth/:id` commands which fetch a single entry
+3. Responds to `POST /doc` API commands which creates an entry
 
 See [spec.md](spec.md) for the working spec.
 
 ## POST /slack
 
-A "slash command" integration with Slack can be used to make a call to `<yoururl>/slack`. The app reads the value of the environment variable `SLACK_TOKEN`. If it matches the incoming token provided by slack, then the "text" field is used to populate the "title" of the event and a template event is created in Cloudant.
+A "slash command" integration with Slack can be used to make a call to `<yoururl>/slack`. The app reads the value of the environment variable `SLACK_TOKEN`. If it matches the incoming token provided by slack, then the "text" field is used to populate the "title" of the event and a token document is created in the 'tokens' database in Cloudant.
   
+A URL is forumulated e.g. /doc/45eb326c52907e2b9d2a81ca000037cc, where '45eb326c52907e2b9d2a81ca000037cc' is the id of the token document.
+
 The API call then returns the URL of where the user can go to edit the event in full.
 
-## GET /doc?id=xxx
+## GET /auth/:id
 
-To be done
+When the user visits the URL, the token document is retrieved which gives the app
 
-## POST /doc?id=xxx
+* the user document
+* the provisional title of the event
+
+This is passed to a Jade form for rendering. The token document is then destroyed (it is a one-time token).
+
+## POST /doc
 
 To be done
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express'),
   config = require('./lib/config.js'),
   moment = require('moment'),
   users = require('./lib/users.js'),
+  tokens = require('./lib/tokens.js'),
   events = require('./lib/events.js'),
   app = express(),
   appEnv = cfenv.getAppEnv(),
@@ -13,6 +14,9 @@ var express = require('express'),
 // body parsing
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// use the jade templating engine
+app.set('view engine', 'jade');
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -46,10 +50,10 @@ app.post('/slack', function(req,res) {
     
     
     users.getOrSave('slack', req.body.user_name, req.body, function(err, data) {
-      obj.attendee = data._id;
-      events.save(obj, function(err, data) {
-        res.send("Thanks for adbocating. Please visit this URL to enter the details https://" + appurl  + "/doc?id="+data.id);
-      });
+      tokens.save({user: data, title: req.body.text}, function(err, data) {
+        res.send("Thanks for advocating. Please visit this URL to enter the details https://" + appurl  + "/auth/"+data.id);
+      })
+
     });
   } else {
     res.send("Invalid request.");   
@@ -57,39 +61,52 @@ app.post('/slack', function(req,res) {
 });
 
 
-app.get("/doc", function(req,res) {
+// authenticate
+app.get("/auth/:id", function(req,res) {
   
   // see which document is to be edited
-  var id = req.query.id;
+  var id = req.params.id;
   if (!id) {
     return res.status(404).send("Invalid id");
   }
   
-  events.load(id, function(err, data) {
+  tokens.load(id, function(err, data) {
     if (err) {
       return res.status(404).send("Missing or unknown id");
     }
     
-    res.send(data);
+    // kill the token - single use only
+    tokens.remove(id, function(e,d) {
+      
+    });
+    
+    // extract the user object from the token
+    var user = data.user;
+    
+    res.render("doc", {doc: { title: data.title}, user: user, });
+//    res.send(data);
     
   })
 });
 
-// set up the database
-cloudant.db.create(config.DBNAME, function(e, d) {
+// set up the databases
+cloudant.db.create(config.TOKEN_DBNAME, function(e,d) {
+  cloudant.db.create(config.DBNAME, function(e, d) {
   
-  // make sure design documents are in place
-  couchmigrate.migrate(config.DBNAME, ddoc, function(e, d) {
+    // make sure design documents are in place
+    couchmigrate.migrate(config.DBNAME, ddoc, function(e, d) {
     
-    // start server on the specified port and binding host
-    app.listen(appEnv.port, '0.0.0.0', function() {
+      // start server on the specified port and binding host
+      app.listen(appEnv.port, '0.0.0.0', function() {
 
-    	// print a message when the server starts listening
-      console.log("server starting on " + appEnv.url);
-    });
+      	// print a message when the server starts listening
+        console.log("server starting on " + appEnv.url);
+      });
     
+    });
   });
 });
+
 
 
 
